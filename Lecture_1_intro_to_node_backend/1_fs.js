49935333018fa3dcf98810ed0048062de34c53fd
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-01-27 15:41:42 +0530

    create, write & update of files & folders

--- Lecture_1_intro_to_node_backend/1_fs.js
+++ Lecture_1_intro_to_node_backend/1_fs.js
@@ -4,25 +4,43 @@
 const fs=require("fs");
 
 /***********to write into a file************/
-let whereToCreateTheFile="file.txt";
+let fileName="file.txt";
 let content="Hello there, the Content added through nodejs";
 
 //synchronous version
 // console.log("```:adding content to the file");
-// fs.writeFileSync(whereToCreateTheFile, content);
+// fs.writeFileSync(fileName, content);
 // console.log("```:added content to the file");
 
 //asynchronous version
-console.log("Before");
+// console.log("Before");
 
-fs.writeFile(whereToCreateTheFile, content, function(err, data){
-    if(err){
-        console.log("something went wrong");
-    }
-    else{
-        console.log("file is written successfully");
-    }
-})
+// fs.writeFile(fileName, content, function(err, data){
+//     if(err){
+//         console.log("something went wrong");
+//     }
+//     else{
+//         console.log("file is written successfully");
+//     }
+// })
 
-console.log("After");
+// console.log("After");
+
+//read file
+// const contentofFile=fs.readFileSync(fileName);
+// console.log("content: "+contentofFile);
+
+//update file
+// fs.appendFileSync(fileName, "Appending my data");
+
+/*****************folders****************/
+// console.log("Creating folder");
+// fs.mkdirSync("lecture-1");
+// console.log("created folder");
+
+
+//deletion of folder
+// console.log("removing lecture-1");
+// fs.rmdirSync("lecture-1");
+// console.log("removed lecture-1");
 
