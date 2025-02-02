1f3ea8cf47200a75f97b7733c368be4fa4efd3c6
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-02-02 16:02:48 +0530

    covered get, post, getAll template route and error handling

--- Lecture_2_Express_api/index.js
+++ Lecture_2_Express_api/index.js
@@ -1,36 +1,88 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
+console.log("Before");
+const content = fs.readFileSync("posts.json", "utf-8");
+const jsonPosts = JSON.parse(content);
+
 //req->request & res->response
-function getHandler(req, res) {
-    console.log("Received get Request");
-    const postData = {
-        "id": 1,
-        "title": "His mother had always taught him",
-        "body": "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
-        "tags": [
-            "history",
-            "american",
-            "crime"
-        ],
-        "reactions": {
-            "likes": 192,
-            "dislikes": 25
-        },
-        "views": 305,
-        "userId": 121
+function getAllPostsHandler(req, res) {
+    try {
+        console.log("Received get Request");
+        res.status(200).json(jsonPosts);
     }
-    res.status(200).json(postData);
+    catch (err) {
+        res.status(200).json({
+            message: "Internal server error"
+        })
+    }
 }
 
-//get request
-app.get("/post", getHandler);
+function getPostById(req, res) {
+    try {
+        const postid = req.params.postId;
+        console.log("postId", postid);
+        const postsArr = jsonPosts.posts
+        for (let i = 0; i < postsArr.length; i++) {
+            if (postsArr[i].id == postid) {
+                return res.status(200).json({
+                    post: postsArr[i]
+                })
+            }
+        }
+        res.status(404).json({
+            post: "post not found"
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            response: "something went wrong on our end"
+        })
+    }
+}
+
+function createPost(req, res) {
+    try {
+        console.log("req.body", req.body);
+        const postsArr = jsonPosts.posts;
+        postsArr.push(req.body);
+        res.status(201).json({
+            message: "post created"
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            response: "something went wrong on our end"
+        })
+    }
+}
+
+function updatePost(){
+
+}
+
+function deletePost(){
+    
+}
+
+app.use(express.json());
+app.post("/posts", createPost);
+
+//getAll request
+app.get("/posts", getAllPostsHandler);
+//get a post
+app.get("/posts/:postId", getPostById);
+//update a post
+app.patch("/posts", updatePost)
+//delete a post
+app.delete("/posts/:/postId", deletePost)
 
 //server start
 app.listen(3000, function () {
     console.log("server is running at port 3000");
 })
-
+console.log("After");
 
 
 
