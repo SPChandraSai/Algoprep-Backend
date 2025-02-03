0f6d9b96f035a096c86f29b1b30f4fc0e371346b
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-02-03 19:30:31 +0530

    Patch and Delete

--- Lecture_2_Express_api/index.js
+++ Lecture_2_Express_api/index.js
@@ -58,25 +58,57 @@ function createPost(req, res) {
     }
 }
 
-function updatePost(){
-
+function updatePost(req, res) {
+    try {
+        console.log("req.body", req.body);
+        const postsArr = jsonPosts.posts;
+        postsArr.push(req.body);
+        res.status(201).json({
+            message: "pushed successfully"
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            response: "something went wrong on our end"
+        })
+    }
 }
 
-function deletePost(){
-    
+function deletePost(req, res) {
+    try {
+        const postid = req.params.postId;
+        console.log("postId", postid);
+        const postsArr = jsonPosts.posts
+        for (let i = 0; i < postsArr.length; i++) {
+            if (postsArr[i].id == postid) {
+                return res.status(200).json({
+                    post: postsArr.splice(i, 1)
+                    // post: postsArr[i]
+                })
+            }
+        }
+        res.status(404).json({
+            post: "post not found"
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            response: "something went wrong on our end"
+        })
+    }
 }
 
 app.use(express.json());
+//post a request
 app.post("/posts", createPost);
-
 //getAll request
 app.get("/posts", getAllPostsHandler);
 //get a post
 app.get("/posts/:postId", getPostById);
-//update a post
+//update a post. Isme banda id btayega vaise but hum body se id lenge islay syntax me id nhi le rehe.
 app.patch("/posts", updatePost)
 //delete a post
-app.delete("/posts/:/postId", deletePost)
+app.delete("/posts/:postId", deletePost)
 
 //server start
 app.listen(3000, function () {
