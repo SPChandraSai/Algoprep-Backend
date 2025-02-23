a1e40b53949963d5439a096efb4f8f9b88580bef
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-02-23 16:54:45 +0530

    mvc and rest api

--- Lecture_7_mvc_rest/api.js
+++ Lecture_7_mvc_rest/api.js
@@ -18,25 +18,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 /*****************auth methods and routes****************/
-const { loginHandler, signupHandler, logoutHandler, protectedRouteMiddleware, profileHandler, isAdminMiddleWare } = require("./controller/authController");
-app.post("/login", loginHandler);
-app.post("/signup", signupHandler);
-app.get("/logout", logoutHandler);
-app.get("/profile", protectedRouteMiddleware, profileHandler);
+const authRouter = require("./router/authRouter.js");
+const userRouter = require("./router/userRouter.js");
+const movieRouter = require("./router/movieRouter.js");
 
-/*************user routes and their handlers*************/
-const { createUser, getAllUser, getUser, deleteUser } = require("./controller/userController");
-app.post("/user", createUser);
-app.get("/user", protectedRouteMiddleware, isAdminMiddleWare, getAllUser);
-app.get("/user/:id", getUser);
-app.delete("/user/:id", protectedRouteMiddleware, deleteUser);
-
-/**********************movies route and handler*******************/
-const { createMovie, getAllMovie, getMovie, deleteMovie } = require("./controller/movieController.js");
-app.post("/movie", createMovie);
-app.get("/movie", protectedRouteMiddleware, isAdminMiddleWare, getAllMovie);
-app.get("/movie/:id", getMovie);
-app.delete("/movie/:id", protectedRouteMiddleware, deleteMovie);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/movie", movieRouter);
 
 app.listen(3000, function () {
     console.log("server started on port 3000")
